759e1263c89513bf424b38b9944dca2517d17cfe
David Amador <david.djlink@gmail.com>
2023-09-03 15:26:43 +0100

    updated to android-19 and removed armeabi ARCH

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi x86 arm64-v8a x86_64
-APP_PLATFORM := android-16
+APP_ABI := armeabi-v7a x86_64 arm64-v8a
+APP_PLATFORM := android-19
 # $(warning APP_ABI $(APP_ABI))
 # $(warning LOCAL_ARM_NEON $(LOCAL_ARM_NEON))
